738e74b1dd80731096271cd65e2275603b3d71e6
Nesiz <NesizenFadilo@gmail.com>
2019-03-11 19:48:11 +0800

    sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 void bubble_sort(int *, int);
+void selection_sort(int *, int);
 void swap(int *, int *);
 
 int main() {
 	int data[] = {12, 1, 7, 45, 25, 1, 23, 5, 9, 0};
 
-    bubble_sort(data, sizeof(data) / sizeof(int));
+    selection_sort(data, sizeof(data) / sizeof(int));
 
     for (size_t i = 0; i < sizeof(data) / sizeof(int); i++) {
 		printf("%d ", data[i]);
@@ -16,15 +17,26 @@ int main() {
 
 void bubble_sort(int *arr, int count) {
 	for (size_t outer = 0; outer < count; outer++) {
-		int swapped = 0;
+		int swapped = 1;
 		for (size_t inner = 0; inner < count; inner++) {
 			if (arr[inner] > arr[inner + 1]) {
 				swap(&arr[inner], &arr[inner + 1]);
-                swapped = 1;
+                swapped = 0;
             }
 		}
-        //if(swapped == 1)
-          //  break;
+        if(swapped)
+            break;
+    }
+}
+
+void selection_sort(int *arr, int count){
+    for (size_t outer = 0; outer < count; outer++){
+        int min = outer;
+        for (size_t inner = outer; inner < count; inner++)
+            if(arr[inner] < arr[min])
+                min = inner;
+        if (min != outer)
+            swap(&arr[min], &arr[outer]);
     }
 }
 
